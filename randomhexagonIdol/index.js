958886f6819e8b854d0b0f301ad0ae3493e246af
mankiww <72963478+mankiww@users.noreply.github.com>
2021-02-06 22:15:13 +0900

    Add files via upload

--- randomhexagonIdol/index.js
+++ randomhexagonIdol/index.js
@@ -1,3 +1,9 @@
+const nameForm = document.querySelector("#nameCard");
+const nameInput = nameForm.querySelector("input");
+const nameSelect = document.querySelector("select");
+const colorForm = document.querySelector("#colorSelector");
+const colorInput = colorForm.querySelector("input");
+
 var randomScalingFactor = function() {
     return Math.floor(Math.random() * 6);
 };
@@ -9,24 +15,9 @@ const memory = document.querySelector("#memory");
 var colorNames = ['red', 'blue', 'green', 'purple'];
 
 var randomColorIndex = Math.floor(Math.random() * colorNames.length);
-var colorIndex = window.chartColors[colorNames[randomColorIndex]]
+var colorIndex = 'skyblue'
 var color = Chart.helpers.color;
-
-var groupName;
-switch (colorNames[randomColorIndex]) {
-    case 'red':
-        groupName = "사쿠라모리";
-        break;
-    case 'blue':
-        groupName = "WHE";
-        break;
-    case 'green':
-        groupName = "미루아이";
-        break;
-    case 'purple':
-        groupName = "네코야나기";
-}
-
+var groupName = "무명"
 
 var config = {
     type: 'radar',
@@ -51,6 +42,8 @@ var config = {
         }]
     },
     options: {
+        responsive: true,
+        maintainAspectRatio: false,
         tooltips: {
             mode: "dataset",
             callbacks: {
@@ -77,46 +70,70 @@ var config = {
     }
 };
 
+function paintSelect (){
+    while(nameSelect.firstChild !== null){
+        nameSelect.removeChild(nameSelect.firstChild);
+    }
+
+    config.data.datasets.forEach(function(dataset){
+        var dataName = dataset.label;
+        console.log(dataName)
+        var nameOption = document.createElement("option");
+        nameOption.value = dataName;
+        nameOption.innerText = dataName;
+        nameSelect.appendChild(nameOption);
+    })    
+}
+
 window.onload = function() {
     window.myRadar = new Chart(document.getElementById('canvas'), config);
+    paintSelect()
 };
 
 document.getElementById('randomizeData').addEventListener('click', function() {
-    config.data.datasets.forEach(function(dataset) {
-        dataset.data = dataset.data.map(function() {
+    var dataIndex = config.data.datasets.findIndex(function(dataset){
+        return dataset.label ===nameSelect.value
+    });
+    
+    var dataset = config.data.datasets[dataIndex];
+    config.data.datasets[dataIndex].data = dataset.data.map(function() {
             return randomScalingFactor();
         });
-    });
-
     window.myRadar.update();
 });
+
+nameForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var dataIndex = config.data.datasets.findIndex(function(dataset){
+        return dataset.label ===nameSelect.value
+    });
+    config.data.datasets[dataIndex].label = nameInput.value;
+    window.myRadar.update();
+    paintSelect()
+})
+
+colorForm.addEventListener("submit", function(event){
+    event.preventDefault();    
+    var newColor = colorInput.value;
+    console.log(newColor);
+    var dataIndex = config.data.datasets.findIndex(function(dataset){
+        return dataset.label ===nameSelect.value
+    }); 
+    var dataset = config.data.datasets[dataIndex];
+    dataset.backgroundColor = color(newColor).alpha(0.2).rgbString();
+    dataset.borderColor = newColor;
+    dataset.pointBackgroundColor = newColor;
+    window.myRadar.update();
+})
 
 var colorNames =  ['red', 'blue', 'green', 'purple'];
 		document.getElementById('addDataset').addEventListener('click', function() {
 			var randomColorIndex = Math.floor(Math.random() * colorNames.length);
-            var newColor = window.chartColors[colorNames[randomColorIndex]]
-            
-            var groupName;
-            switch (colorNames[randomColorIndex]) {
-                case 'red':
-                    groupName = "사쿠라모리";
-                    break;
-                case 'blue':
-                    groupName = "WHE";
-                    break;
-                case 'green':
-                    groupName = "미루아이";
-                    break;
-                case 'purple':
-                    groupName = "네코야나기";
-            }
-            
-            var groupNumber = config.data.datasets.filter(function(element){
-                return element.borderColor === newColor
-            })
+            var newColor = window.chartColors[colorNames[randomColorIndex]]            
+            var groupNumber = config.data.datasets.length
 
 			var newDataset = {
-				label: `${groupName}${groupNumber.length+1}`,
+				label: `${groupName}${groupNumber+1}`,
 				borderColor: newColor,
 				backgroundColor: color(newColor).alpha(0.2).rgbString(),
                 pointBackgroundColor: newColor,                    
@@ -131,7 +148,10 @@ var colorNames =  ['red', 'blue', 'green', 'purple'];
 			}
 
 			config.data.datasets.push(newDataset);
-			window.myRadar.update();
+            window.myRadar.update();
+            
+            paintSelect()
+
         });
         
     const memo = document.querySelector("#memory");
